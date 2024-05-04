69151277e9a911e96c97e16f35c8f6eeac610569
Minju <angertiger0941@gmail.com>
2024-05-04 13:06:04 +0900

    feat: setting 완성
    - 캐러셀 메뉴 완성
    - 테마 및 배경음악에 대한 변수 추가(config.js)
    - 디자인 적용만 하면 끝.

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,8 @@
 var CHAR_LIST = ['A', 'B', 'C']; //캐릭터 파일 이름.
 var CHAR = 0; //현재 선택된 캐릭터 인덱스
+
+var THEME_LIST = ['A', 'B', 'C']; //테마 파일 이름.
+var THEME = 0; //현재 선택된 테마
+
+var BGM_LIST = ['BGM A', 'BGM B', 'BGM C', 'BGM D']; //배경음악 파일 이름.
+var BGM = 0; //현재 선택된 배경음악
--- scripts/setting.js
+++ scripts/setting.js
@@ -1,6 +1,8 @@
 function showSetting(){
     // console.log(document.getElementById('charMenu').children[CHAR], CHAR);
     selectChar(document.getElementById('charMenu').children[CHAR]);
+    createCarouselMenu('bgm', BGM_LIST, "BGM");
+    createCarouselMenu('bgi', THEME_LIST, "THEME");
 }
 
 function selectChar(event){
@@ -15,9 +17,71 @@ function selectChar(event){
     event.classList.add("selected");
     
     var t = CHAR_LIST.indexOf(event.children[0].alt);
-    if(t != CHAR){
-        CHAR = t
-        console.log(CHAR);
+    CHAR = t
+    console.log(CHAR);
+}
+
+var cmMenuWidth = 300;
+
+function createCarouselMenu(pos, content, val){
+    var prev, next, container, cmItems, cmItem, parent;
+
+    parent = document.querySelector(`#${pos} .carouselMenu`);
+    prev = document.createElement('button');
+    prev.style.display = 'block';
+    prev.innerHTML = 'prev';
+    parent.appendChild(prev);
+
+    container = document.createElement('div');
+    container.classList.add('cmContainer');
+    parent.appendChild(container);
+    
+    cmItems = document.createElement('div');
+    cmItems.classList.add('cmItems');
+    container.appendChild(cmItems);
+
+    for(var i = 0; i<content.length; i++){
+        cmItem = document.createElement('div');
+        cmItem.classList.add('cmItem');
+        cmItem.innerHTML = content[i];
+        cmItems.appendChild(cmItem);
+    }
+
+    next = document.createElement('button');
+    next.style.display = 'block';
+    next.innerHTML = 'next';
+    parent.appendChild(next);
+
+    if(val == "THEME"){
+        prev.addEventListener('click', function(){
+            if(THEME>0){
+                THEME-=1;
+                cmItems.style.transform = `translateX(-${THEME*cmMenuWidth}px)`;
+                console.log(THEME);
+            }
+        });
+        next.addEventListener('click', function(){
+            if(THEME<THEME_LIST.length-1){
+                THEME+=1;
+                cmItems.style.transform = `translateX(-${THEME*cmMenuWidth}px)`;
+                console.log(THEME);
+
+            }
+        });
+    } else if(val == "BGM"){
+        prev.addEventListener('click', function(){
+            if(BGM>0){
+                BGM-=1;
+                cmItems.style.transform = `translateX(-${BGM*cmMenuWidth}px)`;
+                console.log(BGM);
+            }
+        });
+        next.addEventListener('click', function(){
+            if(BGM<BGM_LIST.length-1){
+                BGM+=1;
+                cmItems.style.transform = `translateX(-${BGM*cmMenuWidth}px)`;
+                console.log(BGM);
+            }
+        });
     }
 }
-    
