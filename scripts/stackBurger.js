d3117e6f46632774a4ce8396af88f4aa96670e8c
Minju <angertiger0941@gmail.com>
2024-05-05 21:22:35 +0900

    feat: 버거 그리는 함수
    - pickBurgerRecipe(): 주문 생성, 재료 그리는 위치 초기화, 현재의 재료 초기화, 현재 버거 상태 초기화, 버거 레시피 그리기 호출
    - showBurgerRecipe(): 주문 그림을 그림
    - isComplete(): 버거 완성 여부 확인
    - appendIngredient(): 현재 버거에 재료 추가, 호출했을 때 버거가 완성되어 있으면 picBurgerRecipe() 호출.
    -removeIngredient():현재 버거에 재료 빼기

--- scripts/stackBurger.js
+++ scripts/stackBurger.js
@@ -7,14 +7,23 @@ const BURGER_LIST =[
 const BURGER_WIDTH = 180;
 const INGREDIENT = {'bottom-bun': (7*BURGER_WIDTH)/30, 'lettuce':(3*BURGER_WIDTH)/30, 'tomato':(4*BURGER_WIDTH)/30, 'patty':(5*BURGER_WIDTH)/30, 'cheese':(7*BURGER_WIDTH)/30};
 var BURGER = 3;//현재 만들어야하는 버거
-const cheeseMargin = 50;
+const cheeseMargin = 30;
+var ingredientNexth;
+var currentIngredient;
+
+function pickBurgerRecipe(){
+    BURGER = Math.floor(Math.random() * BURGER_LIST.length);
+    ingredientNexth = 30;//초기화
+    currentIngredient = -1;
+    document.getElementById('currentBurger').innerHTML = '';
+    showBurgerRecipe();
+}
 
 function showBurgerRecipe(){
+    //css 관한 부분은 css로 옮겨서 설정해줘도 좋을 것 같음. 택!
     var ingredient, h = 30;
     var answerRecipe = BURGER_LIST[BURGER];
-    var parent = document.getElementById('burgerRecipe');
-    parent.style.width = "300px";
-    var div = document.createElement('div');
+    var div = document.getElementById('burgerRecipe');
     div.innerHTML = "";
     div.style.position = 'relative';
     div.style.width = "300px";
@@ -34,6 +43,52 @@ function showBurgerRecipe(){
         
         div.appendChild(ingredient);
     }
+}
 
-    parent.appendChild(div);
+function isComplete(){
+    var answerRecipe = BURGER_LIST[BURGER];
+    if(currentIngredient == answerRecipe.length-1)
+        return true;
+    return false;
+}
+
+function appendIngredient(){
+    //난이도 별로 판정이 달라서 판정 후에 본 함수를 호출하여 추가하면 됨.
+    var ingredient;
+    var answerRecipe = BURGER_LIST[BURGER];
+    var div = document.getElementById('currentBurger');
+
+    //수정 필요해 보임. 
+    //완성이 되었으면 새로운 레시피 선택하도록 함. 현재의 완성품은 없앰.
+    if(isComplete()){
+        pickBurgerRecipe();
+        return;
+    }
+
+    currentIngredient += 1;
+
+    ingredient = document.createElement('img');
+    ingredient.src = `img/ingredient/${answerRecipe[currentIngredient]}.png`;
+    ingredient.style.position = 'absolute';
+    ingredient.style.width = `${BURGER_WIDTH}px`;
+    if(answerRecipe[currentIngredient] === 'cheese') {
+        ingredientNexth -= cheeseMargin; // 치즈 마진 추가
+    }
+    ingredient.style.bottom = `${ingredientNexth}px`;
+    ingredientNexth += INGREDIENT[answerRecipe[currentIngredient]];
+    
+    div.appendChild(ingredient);
+}
+
+function removeIngredient(){
+    var answerRecipe = BURGER_LIST[BURGER];
+    var div = document.getElementById('currentBurger');
+    if(div.childElementCount>0){
+        div.removeChild(div.lastChild);
+        if(answerRecipe[currentIngredient] === 'cheese') {
+            ingredientNexth += cheeseMargin; // 치즈 마진 추가
+        }
+        ingredientNexth -= INGREDIENT[answerRecipe[currentIngredient]];
+        currentIngredient -= 1;
+    }
 }
