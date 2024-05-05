982c619e596630af69c93a4c15b82fd0a785b61e
Minju <angertiger0941@gmail.com>
2024-05-05 19:06:10 +0900

    fix: 캐러셀 매뉴 버튼 수정
    - 버튼 구조 변경
    -- button -> div
    -- div 하위 이미지 두개

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 var CHAR_LIST = ['A', 'B', 'C']; //캐릭터 파일 이름.
 var CHAR = 0; //현재 선택된 캐릭터 인덱스
 
-var THEME_LIST = ['A', 'B', 'C']; //테마 파일 이름.
+var THEME_LIST = ['Theme A', 'Theme B', 'Theme C']; //테마 파일 이름.
 var THEME = 0; //현재 선택된 테마
 
 var BGM_LIST = ['BGM A', 'BGM B', 'BGM C', 'BGM D']; //배경음악 파일 이름.
