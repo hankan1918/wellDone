924a550152a7ae9db2d13e6a963f67a413aa507c
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-30 09:19:57 +0900

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -4,37 +4,41 @@
 // EASY: 20점 이상
 // NORMAL: 10점 이상 && 버거 카운트 1개 이상
 // HARD: 100초 끝나기 전까지 버거 카운트 1개 이상
-
 function nextMode(){
-    if (mode == MODE.EASY && score >= 50){ // score 변경
+    if (mode == MODE.EASY && score >= 50){
         mode = MODE.NORMAL
         resetGame();
         changeState("굶주린 손님들");
+        setModeClearHistory(MODE.EASY)
         showBeforeNormalPage();
     }
-    if (mode == MODE.NORMAL && score >= 10 && burgerCount >= 1){
+    if (mode == MODE.NORMAL && burgerCount >= 1){
         mode = MODE.HARD
         resetGame();
         changeState("아무튼 버거를 주세요");
+        setModeClearHistory(MODE.NORMAL)
         showBeforeHardPage();
     }
 }
 
-
+/* 시간 0일 때 처리 함수 */
 function gameTimer(){
     remainingTime--;
-    if (remainingTime <= 100){      // remainingTime 변경
+    if (remainingTime <= 100){
         if(charimg != "BENJAMIN"){
             updateTime();
             if(mode != MODE.HARD) {drawGameover();}
+            // mode == MODE.HARD인 경우만, 버거 완성 개수가 3 이상이면 컴플릿, 아니면 게임오버
             else{
-                if (burgerCount >= 1){  // burgerCount 변경
+                if (burgerCount >= 1 && score >= 80){
                     changeState("제대로 된 버거를 줘");
+                    setModeClearHistory(MODE.NORMAL)
                     completeHard();
                 }
                 else {drawGameover();}
                 }
         } else {
+            // 캐릭터가 BENJAMIN인 경우 REMAINGTIME 0 일 때 추가 시간(BENJAMINABILITY) 제공
             bencount--;
             remainingTime = BENJAMINABILITY.time;
             console.log(bencount);
@@ -42,9 +46,9 @@ function gameTimer(){
                 remainingTime = 0;
                 updateTime();
                 if(mode != MODE.HARD) {drawGameover();}
-
+                // mode == MODE.HARD인 경우만, 버거 완성 개수가 3 이상이면 컴플릿, 아니면 게임오버
                 else{
-                    if (burgerCount >= 1){ // burgerCount 변경
+                    if (burgerCount >= 1 && score >= 80){
                         changeState("제대로 된 버거를 줘");
                         completeHard();
                         }
